e81703cd9953494515078a8b905023359071a727
Anne Burt <71862670+anneburt@users.noreply.github.com>
2023-03-08 17:38:18 -0700

    add tutorial popup window

---
+++ main.js
@@ -0,0 +1,12 @@
+function showPopup() {
+    var tutorial = document.getElementById("tutorial");
+    var game = document.getElementById("game");
+
+    if (tutorial.style.display == "none") {
+        tutorial.style.display = "flex";
+        game.style.display = "none";
+    } else {
+        tutorial.style.display = "none";
+        game.style.display = "flex";
+    }
+  }
