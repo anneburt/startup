bc1921e11fcd45f3270c7a50e0a23fe0582e256d
Anne Burt <71862670+anneburt@users.noreply.github.com>
2023-03-08 20:05:51 -0700

    create drag event listeners

--- main.js
+++ main.js
@@ -10,3 +10,33 @@ function showPopup() {
         game.style.display = "flex";
     }
 }
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+    square.addEventListener('dragenter', dragEnter)
+    square.addEventListener('dragover', dragOver);
+    square.addEventListener('dragleave', dragLeave);
+    square.addEventListener('drop', drop);
+});
+
+function dragEnter(e) {
+    e.target.classList.add('drag-over');
+    console.log('drag enter');
+}
+
+function dragOver(e) {
+    e.target.classList.add('drag-over');
+    console.log('drag over');
+}
+
+function dragLeave(e) {
+    e.target.classList.remove('drag-over');
+    console.log('drag leave');
+}
+
+function drop(e) {
+    e.target.classList.remove('drag-over');
+    console.log('drag drop');
+}
+
