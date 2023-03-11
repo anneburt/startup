6d9fd6e1cec3bd6240db7dcce4bcd1e636808d8d
Anne Burt <71862670+anneburt@users.noreply.github.com>
2023-03-10 18:13:32 -0700

    add row clear functionality

--- main.js
+++ main.js
@@ -1,11 +1,92 @@
 const blockNames = ["left-block", "center-block", "right-block"];
+const lightCells = [3,4,5,12,13,14,21,22,23,27,28,29,33,34,35,36,37,38,42,
+    43,44,45,46,47,51,52,53,57,58,59,66,67,68,75,76,77]
+const blockNameToIndex = createBlockNameToIndexMap();
 window.onload = play();
 
-function play(board=generateBoard()) {
-    const blocks = generateBlocks();
-    const blockNameToIndex = createBlockNameToIndexMap();
+function play(board=generateBoard(), blocks=generateBlocks(), blocksLeft=blockNames) {
 
-    createBlockBoardInteraction(blocks, board, blockNameToIndex);
+    // add drag event listeners to each block
+    for(let i = 0; i < blocksLeft.length; i++) {
+        document.getElementById(blocksLeft[i]).addEventListener('dragstart', dragStart);
+    }
+
+    // store current block
+    function dragStart(e) {
+        currentBlock = e.target.id;
+    }
+
+    // add event listeners to each block
+    let cells = document.querySelectorAll(".cell.cell-light, .cell.cell-dark");
+    cells.forEach(cell => {
+        cell.addEventListener('dragover', dragOver);
+        cell.addEventListener('dragleave', dragLeave);
+        cell.addEventListener('drop', dragDrop);
+    });
+
+    function dragOver(e) {
+        console.log("current block:", currentBlock);
+        e.preventDefault();
+
+        const targetCells = getTargetCells(blocks[blockNameToIndex.get(currentBlock)], Number(e.target.id), board);
+        addDragOverClassToTargetCells(targetCells);
+    }
+
+    function dragLeave(e) {
+        console.log("current block:", currentBlock);
+        const targetCells = getTargetCells(blocks[blockNameToIndex.get(currentBlock)], Number(e.target.id), board);
+        removeDragOverClassFromTargetCells(targetCells);
+    }
+
+    function dragDrop(e) {
+        console.log("current block:", currentBlock);
+        currentBlockObject = blocks[blockNameToIndex.get(currentBlock)]
+        const targetCells = getTargetCells(currentBlockObject, Number(e.target.id), board);
+
+        // update board with new block
+        for(let i = 0; i < targetCells.length; i++) {
+            board[targetCells[i]] = 1;
+
+            // document.getElementById(targetCells[i]).removeEventListener('dragstart', dragStart);
+            document.getElementById(targetCells[i]).removeEventListener('dragover', dragOver);
+            document.getElementById(targetCells[i]).removeEventListener('dragleave', dragLeave);
+            document.getElementById(targetCells[i]).removeEventListener('drop', dragDrop);
+
+            document.getElementById(Number(targetCells[i])).className = "cell cell-fill";
+        }
+
+        // remove block from view from block container
+        changeCellClass(currentBlockObject, blockNameToIndex.get(currentBlock), "cell cell-empty");
+
+        updateScore(currentBlockObject);
+
+        blocksLeft = blocksLeft.filter(x => x != currentBlock);
+
+        if(checkClears(board) && blocksLeft.length > 0) {
+            let cells = document.querySelectorAll(".cell.cell-light, .cell.cell-dark");
+            cells.forEach(cell => {
+                // cell.removeEventListener('dragenter', dragEnter);
+                cell.removeEventListener('dragover', dragOver);
+                cell.removeEventListener('dragleave', dragLeave);
+                cell.removeEventListener('drop', dragDrop);
+            });
+
+            play(board, blocks, blocksLeft);
+        }
+
+        // check if all current blocks have been placed
+        if(blocksLeft.length < 1) {
+            // remove event listeners
+            let cells = document.querySelectorAll(".cell.cell-light, .cell.cell-dark");
+            cells.forEach(cell => {
+                cell.removeEventListener('dragover', dragOver);
+                cell.removeEventListener('dragleave', dragLeave);
+                cell.removeEventListener('drop', dragDrop);
+            });
+
+            play(board);
+        }
+    }
 }
 
 function generateBoard() {
@@ -51,87 +132,9 @@ function createBlockNameToIndexMap() {
     return blockNameToIndex;
 }
 
-function createBlockBoardInteraction(blocks, board, blockNameToIndex) {
-    // add drag event listeners to each block
-    for(let i = 0; i < blockNames.length; i++) {
-        document.getElementById(blockNames[i]).addEventListener('dragstart', dragStart);
-    }
-
-    let blocksPlaced = 0;
-
-    // store current block
-    function dragStart(e) {
-        currentBlock = e.target.id;
-    }
-
-    // add event listeners to each block
-    let cells = document.querySelectorAll(".cell.cell-light, .cell.cell-dark");
-    cells.forEach(cell => {
-        cell.addEventListener('dragenter', dragEnter);
-        cell.addEventListener('dragover', dragOver);
-        cell.addEventListener('dragleave', dragLeave);
-        cell.addEventListener('drop', dragDrop);
-    });
-
-    function dragEnter(e) {
-        e.preventDefault();
-
-        const targetCells = getTargetCells(blocks[blockNameToIndex.get(currentBlock)], Number(e.target.id), board);
-        addDragOverClassToTargetCells(targetCells);
-    }
-
-    function dragOver(e) {
-        e.preventDefault();
-
-        const targetCells = getTargetCells(blocks[blockNameToIndex.get(currentBlock)], Number(e.target.id), board);
-        addDragOverClassToTargetCells(targetCells);
-    }
-
-    function dragLeave(e) {
-        const targetCells = getTargetCells(blocks[blockNameToIndex.get(currentBlock)], Number(e.target.id), board);
-        removeDragOverClassFromTargetCells(targetCells);
-    }
-
-    function dragDrop(e) {
-        currentBlockObject = blocks[blockNameToIndex.get(currentBlock)]
-        const targetCells = getTargetCells(currentBlockObject, Number(e.target.id), board);
-
-        // update board with new block
-        for(let i = 0; i < targetCells.length; i++) {
-            board[targetCells[i]] = 1;
-
-            document.getElementById(targetCells[i]).removeEventListener('dragstart', dragStart);
-            document.getElementById(targetCells[i]).removeEventListener('dragover', dragOver);
-            document.getElementById(targetCells[i]).removeEventListener('dragleave', dragLeave);
-            document.getElementById(targetCells[i]).removeEventListener('drop', dragDrop);
-
-            document.getElementById(Number(targetCells[i])).className = "cell cell-fill";
-        }
-
-        // remove block from view from block container
-        changeCellClass(currentBlockObject, blockNameToIndex.get(currentBlock), "cell cell-empty");
-
-        updateScore(currentBlockObject);
-
-        // check if all current blocks have been placed
-        blocksPlaced++;
-        if(blocksPlaced > 2) {
-            // remove event listeners
-            let cells = document.querySelectorAll(".cell.cell-light, .cell.cell-dark");
-            cells.forEach(cell => {
-                cell.removeEventListener('dragenter', dragEnter);
-                cell.removeEventListener('dragover', dragOver);
-                cell.removeEventListener('dragleave', dragLeave);
-                cell.removeEventListener('drop', dragDrop);
-            });
-
-            play(board);
-        }
-    }
-}
-
 function getTargetCells(block, cellId, board) {
-    let calculateTargetCells = [(x) => x - 10,
+    let calculateTargetCells = [
+        (x) => x - 10,
         (x) => x - 9,
         (x) => x - 8,
         (x) => x - 1,
@@ -144,7 +147,7 @@ function getTargetCells(block, cellId, board) {
     let targetCells = []
     for(let i = 0; i < block.length; i++) {
         if(block[i] == 1) {
-            const targetCell = calculateTargetCells[i](cellId);
+            let targetCell = calculateTargetCells[i](cellId);
 
             if(targetCell < 0 || targetCell > 80) {
                 // out of game board range
@@ -184,6 +187,48 @@ function updateScore(block) {
         }
     }
     document.getElementById("score").textContent = score;
+}
+
+function checkClears(board) {
+    let cleared = false;
+    if(checkRowClears(board)) {
+        cleared = true;
+    }
+
+    //TODO: checkColumnClears();
+    //TODO: checkSquareClears();
+    return cleared;
+}
+
+function checkRowClears(board) {
+    cleared = false;
+    for(let row = 0; row < 9; row++) {
+        let needsClear = true
+        for(let column = 0; column < 9; column++) {
+            if(board[column + (9 * row)] == 0) {
+                needsClear = false;
+            }
+        }
+
+        if(needsClear) {
+            clearRow(row, board);
+            cleared = true;
+        }
+    }
+    return cleared;
+}
+
+function clearRow(rowIndex, board) {
+    for(let i = 0; i < 9; i++) {
+        let targetCell = i +(9 * rowIndex);
+        board[targetCell] = 0;
+
+        if(lightCells.includes(targetCell)) {
+            document.getElementById(targetCell).className = "cell cell-light";
+        } else {
+            document.getElementById(targetCell).className = "cell cell-dark";
+        }
+    }
 }
 
 function reset() {
